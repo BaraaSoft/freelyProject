f31696e2ec56572534ca12b0ad4682a1162c7f84
BaraaSoft <baraa.abuzaid@eatfirst.com.au>
2022-08-20 17:52:35 +1000

    load trip details from redux

--- src/screens/tripDetails/TripDetails.tsx
+++ src/screens/tripDetails/TripDetails.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useMemo} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {goTo, navigate} from '../../services/navigationService';
 import {APP_SCREENS} from '../screens';
 import {Container, Title, Link} from './TripDetails.style';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../store';
 
 export const navigationOptions = ({navigation, route}: any) => {
   const {tripId, tripName} = route.params;
@@ -12,10 +14,22 @@ export const navigationOptions = ({navigation, route}: any) => {
     isVisible: false,
   };
 };
-const TripDetails = () => {
+const TripDetails = ({navigation, route}: any) => {
+  const {data, isError, loading} = useSelector(
+    (state: RootState) => state.trips,
+  );
+  const {destinations} = useMemo(() => {
+    const {tripId, tripName} = route.params;
+    return data.filter((x: any) => x.id == tripId)[0];
+  }, [route.params?.tripId]);
+
   return (
     <Container>
-      <Title>Trips screen</Title>
+      <View>
+        {destinations.map((name: string) => (
+          <Title>{name}</Title>
+        ))}
+      </View>
     </Container>
   );
 };
